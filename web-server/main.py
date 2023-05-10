8c19edf342b362ac7dfa1077243c7c62b51e4bf8
cristian <cristian_perez_1996@hotmail.com>
2023-05-09 20:55:42 -0600

    Use fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,45,5,13]
+
+@app.get('/contact',response_class=HTMLResponse)
+def get_list():
+    return """ 
+            <h1> Hola soy una pagina </h1>
+            <p> Soy un parrafo </p>
+      
+        """
 
 def run():
     store.get_categories()
